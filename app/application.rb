14ac6a226e2e0e65c29efe9c4c11b2e893b6829b
zachary mines <zmines@wesleyan.edu>
2020-08-31 03:19:30 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,9 +1,6 @@
 
 class Application 
   def call(env)
-     return [ 200, {'Content-Type' => 'text/html'}, pretty_response ]
-  end
-  def pretty_response
     if (Time.now.to_i) < 12 
       resp.write "Good Morning!" 
     else 
