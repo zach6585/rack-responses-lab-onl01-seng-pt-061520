36de600d6e9afb604516c4a2c835d93207ec4f0e
zachary mines <zmines@wesleyan.edu>
2020-08-31 03:26:31 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -6,7 +6,7 @@ class Application
       resp.write "#{Time.now.to_s[12...]}"
       resp.write "Good Morning!" 
     else 
-      resp.write "#{Time.now[12...]}"
+      resp.write "#{Time.now.to_s[12...]}"
       # resp.write "Good Afternoon!"
     end 
     resp.finish
