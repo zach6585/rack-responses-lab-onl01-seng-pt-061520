eb0c6d1da4bd9a20d2ea17783c2754adf0acaed3
zachary mines <zmines@wesleyan.edu>
2020-08-31 03:27:08 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -2,7 +2,7 @@
 class Application 
   def call(env)
     resp = Rack::Response.new
-    if (Time.now.to_i) < 12 
+    if (Time.now.to_s[12...]) < 12 
       resp.write "#{Time.now.to_s[12...]}"
       resp.write "Good Morning!" 
     else 
