4e5179556a3e4e14e65b23af7269e0bef119fbfa
zachary mines <zmines@wesleyan.edu>
2020-08-31 03:27:51 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,7 +3,6 @@ class Application
   def call(env)
     resp = Rack::Response.new
     if Time.now.to_s[13] == ':'
-      resp.write "#{Time.now.to_s[12...]}"
       resp.write "Good Morning!" 
     else 
       resp.write "#{Time.now.to_s[12...]}"
