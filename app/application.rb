49cdc1ba7385bd5df6df4f1cdad3b47a1f64e9fb
zachary mines <zmines@wesleyan.edu>
2020-08-31 03:32:34 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -9,7 +9,9 @@ class Application
       resp.write "#{Time.now.to_s[12...14].to_i < 12}"
       resp.write "Good Morning!"
     elsif Time.now.to_s[12...14].to_i >= 12 
+      resp.write "#{Time.now.to_s[12...14].to_i >= 12}"
       resp.write "Good Afternoon!"
+      
     end 
     resp.finish
   end
